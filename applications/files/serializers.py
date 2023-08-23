c981a6635a9cc1f772fe5b6b5962d9780fb971fe
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-08-23 15:00:56 -0500

    Add file name as read only field

--- applications/files/serializers.py
+++ applications/files/serializers.py
@@ -1,9 +1,15 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 from .models import File
 
 
-class FileModelSerializer(ModelSerializer):
+class FileModelSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the File model.
+    """
+
+    file_name = serializers.CharField(read_only=True, source="get_file_name")
+
     class Meta:
         model = File
         fields = "__all__"
